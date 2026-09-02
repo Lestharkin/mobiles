d0afc0afdf28ac14692c9f64afe9b57510a6b084
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2026-09-02 16:02:40 -0500

    sync

--- sancho/app/src/main/java/edu/upb/sancho/MainActivity.kt
+++ sancho/app/src/main/java/edu/upb/sancho/MainActivity.kt
@@ -1,47 +1,37 @@
 package edu.upb.sancho
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import edu.upb.sancho.ui.theme.SanchoTheme
+import edu.upb.sancho.components.TextViewPersonal
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var mainLayout: LinearLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            SanchoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.main_activity)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        mainLayout = findViewById(R.id.mainLayout)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SanchoTheme {
-        Greeting("Android")
+        /*
+        val textView = TextView(this)
+        textView.text = getString(R.string.hello_world)
+        textView.setTextColor(getColor(R.color.red_500))
+        textView.textAlignment = TextView.TEXT_ALIGNMENT_TEXT_END
+
+        mainLayout.addView(textView)
+        */
+        val textViewPersonal = TextViewPersonal(this)
+        val textViewPersonal2 = TextViewPersonal(this)
+        textViewPersonal2.setBackgroundColor(Color.parseColor("#349854"))
+        mainLayout.addView(textViewPersonal)
+        mainLayout.addView(textViewPersonal2)
     }
+
 }
--- sancho/app/src/main/java/edu/upb/sancho/components/TextViewPersonal.kt
+++ sancho/app/src/main/java/edu/upb/sancho/components/TextViewPersonal.kt
@@ -1,4 +1,14 @@
 package edu.upb.sancho.components
 
-class TextViewPersonal {
+import android.content.Context
+import android.widget.TextView
+import edu.upb.sancho.R
+
+class TextViewPersonal : TextView {
+
+    constructor(context: Context): super(context) {
+        this.text = resources.getText(R.string.hello_world)
+        this.setTextColor(resources.getColor(R.color.red_500))
+        this.textAlignment = TextView.TEXT_ALIGNMENT_TEXT_END
+    }
 }
